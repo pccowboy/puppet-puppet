1841872b7e7d041709f4d90e61944f5649140bf6
David Swift <dswift@sugarinc.com>
2012-03-05 11:53:27 -0800

    Add mysql gem to puppetmaster packages

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,3 +1,5 @@
 class puppet::server::install {
   package { 'puppet-server': ensure => installed, require => Yumrepo["puppetlabs"] }
+  package { 'ruby-devel': ensure => '1.8.7.352'}
+  package { 'mysql': ensure => installed, provider => gem }
 }
