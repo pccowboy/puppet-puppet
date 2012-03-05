f140a5d65a61e870e315ecbd796fb1a102ca2129
David Swift <dswift@sugarinc.com>
2012-03-05 13:32:06 -0800

    Add mysql module calls to create the puppet db and assign users/passwords.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -2,4 +2,18 @@ class puppet::server::install {
   package { 'puppet-server': ensure => installed, require => Yumrepo["puppetlabs"] }
   package { 'ruby-devel': ensure => '1.8.7.352'}
   package { 'mysql': ensure => installed, provider => gem }
+  
+  class { 'mysql::server':
+  	config_hash => { 'root_password' => 'password' }
+  }
+
+  class { 'mysql': }
+  
+  mysql::db { 'puppet':
+	  user     => 'puppet@localhost',
+	  password => 'password',
+	  host     => 'localhost',
+	  grant    => ['all'],
+	  sql      => 'create index exported_restype_title on resources (exported, restype, title(50));',
+	}
 }
