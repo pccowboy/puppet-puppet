1cc802bcbe0d8f369bd3365019df42349da987a6
David Swift <dswift@sugarinc.com>
2012-03-06 15:37:57 -0800

    Switch to mysql2 gem to escape stupid name conflict.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,7 +3,7 @@ class puppet::server::install {
   package { 'mysql-devel': ensure => installed }
   package { 'ruby-devel': ensure => '1.8.7.352-1.8.amzn1'}
   package { 'activerecord': ensure => installed, provider => gem }
-  package { 'mysql': ensure => installed, provider => gem }
+  package { 'mysql2': ensure => installed, provider => gem }
   
   service { "puppet":
   	enable 	=> true,
