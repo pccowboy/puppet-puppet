209229d78416e344f068fa95cd7b36f0cb2c7f42
David Swift <dswift@sugarinc.com>
2012-03-06 13:35:35 -0800

    add activerecord mysql adapter.  move puppet service config to puppet manifest.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,6 +3,21 @@ class puppet::server::install {
   package { 'mysql-devel': ensure => installed }
   package { 'ruby-devel': ensure => '1.8.7.352-1.8.amzn1'}
   package { 'activerecord': ensure => installed, provider => gem }
+  package { 'activerecord-mysql-adapter': ensure => installed, provider => gem }
+  
+  service { "puppet":
+  	enable 	=> true,
+  	ensure 	=> running,
+  	name 	=> "puppet",
+  	require => [Package["puppet"], Package["mysql"], Service["puppetmaster"]],
+  }
+  
+  service { "puppetmaster":
+  	enable 	=> false,
+  	ensure 	=> stopped,
+  	name 	=> "puppetmaster",
+  	require => [Package["puppet-server"], Package["mysql"]],
+  }
   
   class { 'mysql::server':
   	config_hash => { 'root_password' => 'password' }
