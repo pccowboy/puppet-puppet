403523a9e15227979a70bd816687343c0f713bc5
David Swift <dswift@sugarinc.com>
2012-03-07 11:27:55 -0800

    Remove circular dep.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -36,7 +36,6 @@ class puppet::server::install {
     
   class { 'mysql::server':
   	config_hash => { 'root_password' => 'password' },
-  	require => Service["mysqld"],
   }
 
   class { 'mysql': }
