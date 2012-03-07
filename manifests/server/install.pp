d7e09ea32ff847373c9f6cfa8279a7d3ef068e53
David Swift <dswift@sugarinc.com>
2012-03-07 12:49:29 -0800

    give a full path to puppetd

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -20,7 +20,7 @@ class puppet::server::install {
   }
 
   exec { "puppetmaster-run-once":
-    command => "/etc/init.d/puppetmaster start && puppetd -d -t && /etc/init.d/puppetmaster stop",
+    command => "/etc/init.d/puppetmaster start && /usr/sbin/puppetd -d -t && /etc/init.d/puppetmaster stop",
     path	=> "/bin:/usr/bin:/usr/sbin",
     creates => "/var/lib/puppet/ssl/certs/puppet.${domain}.pem",
     require => [Service["puppetmaster"], mysql::db["puppet"]],
